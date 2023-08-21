1bb8d8e6f2c3a24514e65774f7a5742cac1ef8d6
Diego Artiles <diego45882@hotmail.com>
2023-08-20 22:32:42 -0300

    improve the styles

--- apps/astro/src/components/button.tsx
+++ apps/astro/src/components/button.tsx
@@ -6,10 +6,10 @@ type ButtonProps<C extends ElementType> = {
     children: React.ReactNode;
 } & ComponentPropsWithoutRef<C>
 
-function Button <C extends ElementType>({ children, as }: ButtonProps<C>) {
+function Button <C extends ElementType>({ children, as, ...props }: ButtonProps<C>) {
     const Component = as || 'button';
     
-    return <Component className="inline-block text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:focus:ring-gray-500 mr-2 mb-2 w-full">
+    return <Component className="inline-block text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-full text-sm px-5 py-2.5 text-center dark:focus:ring-gray-500 mr-2 mb-2 w-full h-10" {...props}>
         { children }
     </Component>
 }
