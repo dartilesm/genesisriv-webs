34b2ac090eebff5a08c9c15e73c231d882ac9281
Diego Artiles <diego45882@hotmail.com>
2023-08-21 00:17:49 -0300

    update schemas and UI

--- apps/sanity/schemas/link-tree.ts
+++ apps/sanity/schemas/link-tree.ts
@@ -1,51 +1,114 @@
-export default {
-    name: 'linkTree',
-    type: 'document',
-    title: 'Link Tree',
-    fields: [
-      {
-        name: 'photo',
-        type: 'image',
-        title: 'Photo'
+import { DocumentDefinition, Rule } from "sanity";
+
+const linkTree: DocumentDefinition = {
+  name: 'linkTree',
+  type: 'document',
+  title: 'Link Tree',
+  fields: [
+    {
+      name: 'photo',
+      type: 'image',
+      title: 'Photo',
+      options: {
+        hotspot: true
       },
-      {
-        name: 'name',
-        type: 'string',
-        title: 'Name'
-      },
-      {
-        name: 'role',
-        type: 'string',
-        title: 'Role'
-      }, 
-      {
-        name: 'description',
-        type: 'text',
-        title: 'Description'
-      },
-      {
-        name: 'links',
-        type: 'array',
-        title: 'Links',
-        of: [
-          {
-            name: 'link',
-            type: 'object',
-            title: 'Link',
-            fields: [
-              {
-                name: 'name',
-                type: 'string',
-                title: 'Name'
-              },
-              {
-                name: 'url',
-                type: 'url',
-                title: 'URL'
-              }
-            ]
-          }
-        ]
-      }
-    ]
-  }
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alt Text',
+          validation: Rule => Rule.required()
+        }
+      ]
+    },
+    {
+      name: 'name',
+      type: 'string',
+      title: 'Name'
+    },
+    {
+      name: 'role',
+      type: 'string',
+      title: 'Role'
+    }, 
+    {
+      name: 'description',
+      type: 'text',
+      title: 'Description'
+    },
+    {
+      name: 'links',
+      type: 'array',
+      title: 'Links',
+      of: [
+        {
+          name: 'link',
+          type: 'object',
+          title: 'Link',
+          fields: [
+            {
+              name: 'name',
+              type: 'string',
+              title: 'Name',
+              validation: (Rule: Rule) => Rule.required()
+            },
+            {
+              name: 'url',
+              type: 'url',
+              title: 'URL',
+              validation: (Rule: Rule) => Rule.required()
+            },
+            {
+              name: 'ariaLabel',
+              type: 'string',
+              title: 'Aria Label',
+              validation: (Rule: Rule) => Rule.required()
+            }
+          ]
+        }
+      ]
+    },
+    {
+      name: 'iconLinks',
+      type: 'object',
+      title: 'Icon Links',
+      fields: [
+        {
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: Rule => Rule.required()
+        },
+        {
+          name: 'links',
+          type: 'array',
+          title: 'Links',
+          of: [
+            {
+              name: 'icon',
+              type: 'image',
+              title: 'Icon',
+              validation: Rule => Rule.required(),
+              fields: [
+                {
+                  name: 'alt',
+                  type: 'string',
+                  title: 'Alt Text',
+                  validation: Rule => Rule.required()
+                },
+                {
+                  name: 'url',
+                  type: 'url',
+                  title: 'URL',
+                  validation: Rule => Rule.required()
+                }
+              ]
+            },
+          ]
+        }
+      ]
+    }
+  ]
+}
+
+export default linkTree
