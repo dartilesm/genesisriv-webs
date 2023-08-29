0e26b003c52588fd270188faff2da7819e51c0e1
Diego Artiles <diego45882@hotmail.com>
2023-08-29 14:34:42 -0300

    add light mode as default theme, change buttons colors and improve UI

--- apps/astro/src/components/button.tsx
+++ apps/astro/src/components/button.tsx
@@ -4,11 +4,11 @@ type ButtonProps<C extends ElementType> = {
     as?: C;
     children: React.ReactNode;
 } & ComponentPropsWithoutRef<C>
-
+ 
 function Button <C extends ElementType>({ children, as, ...props }: ButtonProps<C>) {
     const Component = as || 'button';
 
-    return <Component className="inline-block text-[#FCFCFC] bg-[#A25AFF] hover:bg-[#6317DA] hover:shadow-[0_4px_10px_0_rgba(53,0,122,0.20)] focus:bg-[#6317DA] focus:outline-4 focus:outline-offset-4 focus:outline-[#D9BCFE] font-semibold rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full h-10 " {...props}>
+    return <Component className="inline-block text-[#5C17B6] bg-[#D4B3FF] hover:bg-[#6D25CA] hover:text-[#FDFDFD] hover:shadow-[0_4px_10px_0_rgba(53,0,122,0.20)] focus:bg-[#6317DA] focus:outline-4 focus:outline-offset-4 focus:outline-[#EAD9FF] focus:text-[#FDFDFD] font-semibold rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full h-10 " {...props}>
         { children }
     </Component>
 }
