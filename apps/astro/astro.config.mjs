87a3b26e7524468eed3de5a0edd558b01adfa729
Diego Artiles <diego45882@hotmail.com>
2023-08-30 13:57:10 -0300

    update to astro v3

--- apps/astro/astro.config.mjs
+++ apps/astro/astro.config.mjs
@@ -1,21 +1,18 @@
-import { defineConfig } from 'astro/config';
 import react from "@astrojs/react";
-import sanity from "@sanity/astro";
 import tailwind from "@astrojs/tailwind";
 import vercel from '@astrojs/vercel/serverless';
+import sanity from "@sanity/astro";
+import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  experimental: {
-    assets: true,
-  },
   integrations: [
     sanity({
       projectId: import.meta.env.VITE_SANITY_PROJECT_ID,
       dataset: import.meta.env.VITE_SANITY_DATASET,
       apiVersion: "v2023-03-01",
       useCdn: true
-    }), 
+    }),
     react(),
     tailwind()
   ],
