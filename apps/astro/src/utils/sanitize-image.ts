e606cacbdc2d1d8b5f0aafb99f90235e4ecd1d77
Diego Artiles <diego45882@hotmail.com>
2023-08-30 01:35:17 -0300

    improve svg/image quality adnd format code

--- apps/astro/src/utils/sanitize-image.ts
+++ apps/astro/src/utils/sanitize-image.ts
@@ -1,9 +1,13 @@
 import { useSanityClient } from "@sanity/astro";
 import imageUrlBuilder from '@sanity/image-url'
-import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
+import type { ImageFormat, SanityImageSource } from "@sanity/image-url/lib/types/types";
 
-function sanitizeImage(source: SanityImageSource) {
-    return imageUrlBuilder(useSanityClient()).image(source).format('webp');
+type options = {
+    format: ImageFormat
+}
+
+function sanitizeImage(source: SanityImageSource, { format }: options = {} as options) {
+    return imageUrlBuilder(useSanityClient()).image(source).format(format);
 }
 
 
