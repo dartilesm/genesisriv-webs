6a4745a392d9e494a757db6d06d5f901b9b74ecc
Diego Artiles <diego45882@hotmail.com>
2023-09-01 18:39:30 -0300

    update styles

--- apps/astro/src/components/button.tsx
+++ apps/astro/src/components/button.tsx
@@ -8,7 +8,7 @@ type ButtonProps<C extends ElementType> = {
 function Button <C extends ElementType>({ children, as, ...props }: ButtonProps<C>) {
     const Component = as || 'button';
 
-    return <Component className="inline-block text-[#5C17B6] bg-[#D4B3FF] hover:bg-[#6D25CA] hover:text-[#FDFDFD] hover:shadow-[0_4px_10px_0_rgba(53,0,122,0.20)] focus:bg-[#6317DA] focus:outline-4 focus:outline-offset-4 focus:outline-[#EAD9FF] focus:text-[#FDFDFD] font-semibold rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full h-10 " {...props}>
+    return <Component className="inline-block text-[#4D05AB] bg-[#D4B3FF] hover:bg-[#5D10C1] hover:text-[#FEFEFE] hover:shadow-[0_4px_10px_0_rgba(53,0,122,0.20)] focus:bg-[#5D10C1] focus:outline-4 focus:outline-offset-4 focus:outline-[#EAD9FF] focus:text-[#FEFEFE] font-semibold rounded-full text-sm px-5 py-2.5 text-center mr-2 mb-2 w-full h-10 " {...props}>
         { children }
     </Component>
 }
