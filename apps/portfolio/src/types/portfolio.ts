d4abb7ee9cb57335a5b0b7642a20c328f1228836
Diego Artiles <diego45882@hotmail.com>
2024-02-03 22:29:27 -0300

    add portfolio schema and connect components

---
+++ apps/portfolio/src/types/portfolio.ts
@@ -0,0 +1,5 @@
+import type { SanityValues } from "@sanity-app/sanity.config";
+
+type Portfolio = SanityValues["portfolio"];
+
+export default Portfolio;
---
+++ apps/sanity/schemas/portfolio.ts
@@ -0,0 +1,330 @@
+import { defineArrayMember, defineField, defineType } from '@sanity-typed/types'
+
+const portfolio = defineType({
+  name: 'portfolio',
+  type: 'document',
+  title: 'Portfolio',
+  fields: [
+    defineField({
+      name: 'presentation',
+      type: 'object',
+      title: 'Presentation',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'description',
+          type: 'array',
+          title: 'Description',
+          of: [
+            defineArrayMember({
+              type: 'block',
+            }),
+          ],
+        }),
+        defineField({
+          name: 'cta',
+          type: 'object',
+          title: 'Call to Action',
+          fields: [
+            defineField({
+              name: 'label',
+              type: 'string',
+              title: 'Label',
+              validation: (Rule) => Rule.required(),
+            }),
+            defineField({
+              name: 'url',
+              type: 'url',
+              title: 'URL',
+              validation: (Rule) => Rule.required(),
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'projects',
+      type: 'object',
+      title: 'Projects',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'description',
+          type: 'string',
+          title: 'Description',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'projectsList',
+          type: 'array',
+          title: 'Projects List',
+          of: [
+            defineArrayMember({
+              name: 'project',
+              type: 'object',
+              title: 'project',
+              fields: [
+                defineField({
+                  name: 'projectName',
+                  type: 'string',
+                  title: 'Project name',
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: 'company',
+                  type: 'string',
+                  title: 'Company',
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: 'description',
+                  type: 'array',
+                  title: 'Description',
+                  of: [
+                    defineArrayMember({
+                      type: 'block',
+                    }),
+                  ],
+                }),
+                defineField({
+                  name: 'cta',
+                  type: 'object',
+                  title: 'Call to Action',
+                  fields: [
+                    defineField({
+                      name: 'label',
+                      type: 'string',
+                      title: 'Label',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                    defineField({
+                      name: 'url',
+                      type: 'url',
+                      title: 'URL',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+                defineField({
+                  name: 'photo',
+                  type: 'image',
+                  title: 'Photo',
+                  options: {
+                    hotspot: true,
+                  },
+                  fields: [
+                    defineField({
+                      name: 'alt',
+                      type: 'string',
+                      title: 'Alt Text',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'experience',
+      type: 'object',
+      title: 'Experience',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'description',
+          type: 'string',
+          title: 'Description',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'experienceList',
+          type: 'array',
+          title: 'Experience List',
+          of: [
+            defineArrayMember({
+              name: 'experience',
+              type: 'object',
+              title: 'Experience',
+              fields: [
+                defineField({
+                  name: 'role',
+                  type: 'string',
+                  title: 'Role',
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: 'subtitle',
+                  type: 'string',
+                  title: 'Subtitle',
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: 'description',
+                  type: 'array',
+                  title: 'Description',
+                  of: [
+                    defineArrayMember({
+                      type: 'block',
+                    }),
+                  ],
+                }),
+                defineField({
+                  name: 'cta',
+                  type: 'object',
+                  title: 'Call to Action',
+                  fields: [
+                    defineField({
+                      name: 'label',
+                      type: 'string',
+                      title: 'Label',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                    defineField({
+                      name: 'url',
+                      type: 'url',
+                      title: 'URL',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+                defineField({
+                  name: 'photo',
+                  type: 'image',
+                  title: 'Photo',
+                  options: {
+                    hotspot: true,
+                  },
+                  fields: [
+                    defineField({
+                      name: 'alt',
+                      type: 'string',
+                      title: 'Alt Text',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'about',
+      type: 'object',
+      title: 'About',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'description',
+          type: 'array',
+          title: 'Description',
+          of: [
+            defineArrayMember({
+              type: 'block',
+            }),
+          ],
+        }),
+      ],
+    }),
+    defineField({
+      name: 'certificates',
+      type: 'object',
+      title: 'Certificates',
+      fields: [
+        defineField({
+          name: 'title',
+          type: 'string',
+          title: 'Title',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'description',
+          type: 'string',
+          title: 'Description',
+          validation: (Rule) => Rule.required(),
+        }),
+        defineField({
+          name: 'certificatesList',
+          type: 'array',
+          title: 'Certificates List',
+          of: [
+            defineArrayMember({
+              name: 'certificate',
+              type: 'object',
+              title: 'Certificate',
+              fields: [
+                defineField({
+                  name: 'title',
+                  type: 'string',
+                  title: 'Title',
+                  validation: (Rule) => Rule.required(),
+                }),
+                defineField({
+                  name: 'cta',
+                  type: 'object',
+                  title: 'Call to Action',
+                  fields: [
+                    defineField({
+                      name: 'label',
+                      type: 'string',
+                      title: 'Label',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                    defineField({
+                      name: 'url',
+                      type: 'url',
+                      title: 'URL',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+                defineField({
+                  name: 'photo',
+                  type: 'image',
+                  title: 'Photo',
+                  options: {
+                    hotspot: true,
+                  },
+                  fields: [
+                    defineField({
+                      name: 'alt',
+                      type: 'string',
+                      title: 'Alt Text',
+                      validation: (Rule) => Rule.required(),
+                    }),
+                  ],
+                }),
+              ],
+            }),
+          ],
+        }),
+      ],
+    }),
+  ],
+})
+
+export default portfolio
