c6459fb356470629359768093683d49b813e83be
Diego Artiles <diego@dartiles.dev>
2024-04-07 15:49:09 -0300

    feat(apps/portfolio): improve mobile UX and add mobile menu

--- apps/portfolio/src/components/header.svelte
+++ apps/portfolio/src/components/header.svelte
@@ -1,14 +1,28 @@
 <script lang="ts">
-  import { Portfolio } from "@/types";
+  import type { Portfolio } from "@/types";
+  import { cn } from "@/utils/cn";
   import sanitizeImage from "@sanity-app/utils/sanitize-image";
   import type { SanityImageSource } from "@sanity/image-url/lib/types/types";
 
   export let data = {} as Portfolio["header"];
 
-  let logoUrl = sanitizeImage(data?.logo as SanityImageSource, { format: "webp" }).url();
+  let logoUrl: string = sanitizeImage(data?.logo as SanityImageSource, { format: "webp" }).url();
+  let isMenuOpen = false;
+
+  const isMobile = window.matchMedia("(max-width: 767px)").matches;
+
+  function handleMenuToggle() {
+    isMenuOpen = !isMenuOpen;
+    if (isMobile) window.document.body.style.overflow = isMenuOpen ? "hidden" : "auto";
+  }
 </script>
 
-<header class="container px-4 md:px-6 mx-auto flex flex-col gap-12">
+<header
+  class={cn("px-4 md:px-6 mx-auto flex flex-col gap-12 z-20 relative", {
+    "bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80":
+      isMobile && isMenuOpen,
+  })}
+>
   <nav class="bg-transparent py-4">
     <div class="max-w-5xl flex flex-wrap items-center justify-between mx-auto">
       <a href="/" class="flex items-center space-x-3 rtl:space-x-reverse">
@@ -20,6 +34,7 @@
         class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
         aria-controls="navbar-default"
         aria-expanded="false"
+        on:click={handleMenuToggle}
       >
         <span class="sr-only">Open main menu</span>
         <svg
@@ -38,16 +53,32 @@
           ></path>
         </svg>
       </button>
-      <div class="hidden w-full md:block md:w-auto" id="navbar-default">
+      <div
+        class={cn({
+          "w-full md:block md:w-auto": !isMobile,
+          "fixed w-full left-0 top-16 bg-white bg-clip-padding backdrop-filter backdrop-blur-sm bg-opacity-80 h-screen z-20 py-4":
+            isMobile,
+        })}
+        class:hidden={!isMenuOpen}
+        id="navbar-default"
+      >
         <ul
-          class="font-medium flex flex-col md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0"
+          class={cn({
+            "font-medium flex flex-col md:p-0 mt-4 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0":
+              !isMobile,
+            "text-right md:text-left px-4 md:px-6 mx-auto flex flex-col gap-4 font-semibold":
+              isMobile,
+          })}
         >
           {#each data?.links || [] as link}
             <li>
               <a
                 href={link.url}
-                class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-purple-700 md:p-0 dark:text-white md:dark:hover:text-purple-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
+                class={cn(
+                  "block py-2 px-3 text-gray-900 rounded hover:bg-transparent border-0 hover:text-purple-700 p-0"
+                )}
                 aria-current="page"
+                on:click={handleMenuToggle}
               >
                 {link.label}
               </a>
