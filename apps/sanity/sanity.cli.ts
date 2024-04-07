50f276dd35a20045c044c50ea604f76cd2fea273
Diego Artiles <diego@dartiles.dev>
2024-04-07 15:49:42 -0300

    feat(apps/sanity): improve environment variables access

--- apps/sanity/sanity.cli.ts
+++ apps/sanity/sanity.cli.ts
@@ -1,8 +1,8 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli'
 
 export default defineCliConfig({
   api: {
-    projectId: process.env.SANITY_PROJECT_ID,
-    dataset: process.env.SANITY_DATASET,
+    projectId: process.env.SANITY_STUDIO_SANITY_PROJECT_ID,
+    dataset: process.env.SANITY_STUDIO_SANITY_DATASET,
   }
 })
