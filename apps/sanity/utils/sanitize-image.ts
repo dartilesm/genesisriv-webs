59768810427aabcb4b4790df134b0719732d8db1
Diego Artiles <diego@dartiles.dev>
2024-04-07 15:49:42 -0300

    feat(apps/sanity): improve environment variables access

--- apps/sanity/utils/sanitize-image.ts
+++ apps/sanity/utils/sanitize-image.ts
@@ -1,9 +1,14 @@
-import { useSanityClient as createSanityClient } from "@sanity/astro";
+import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 import type {
   ImageFormat,
   SanityImageSource,
 } from "@sanity/image-url/lib/types/types";
+
+const client = createClient({
+  projectId: import.meta.env.SANITY_STUDIO_SANITY_PROJECT_ID || import.meta.env.PUBLIC_SANITY_PROJECT_ID,
+  dataset: import.meta.env.SANITY_STUDIO_SANITY_DATASET || import.meta.env.PUBLIC_SANITY_DATASET,
+});
 
 type options = {
   format: ImageFormat;
@@ -13,7 +18,7 @@ function sanitizeImage(
   source: SanityImageSource,
   { format }: options = {} as options,
 ) {
-  return imageUrlBuilder(createSanityClient()).image(source).format(format);
+  return imageUrlBuilder(client).image(source).format(format);
 }
 
 export default sanitizeImage;
